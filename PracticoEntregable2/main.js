aec1147b11d3af81abcbf132887ac8384d0cae95
franmurillo94 <franmurillo1994@gmail.com>
2022-10-10 16:13:57 -0300

    porcentaje al loading

--- PracticoEntregable2/main.js
+++ PracticoEntregable2/main.js
@@ -214,3 +214,14 @@ setTimeout(function(){
     load_container.classList.add('close_load');
 }, 5000);
 
+let load_cont = 0;
+let load_number = document.getElementById("load_number");
+
+setInterval(()=>{
+if(load_cont==100){
+    clearInterval();
+} else {
+    load_cont += 1;
+    load_number.textContent = load_cont + "%";
+}
+},40);
